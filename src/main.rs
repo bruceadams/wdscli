73eca8487c6e0bbf2bc5699a23546a1af70fefcb
Bruce Adams <bruce.adams@acm.org>
2017-01-10 16:47:50 -0500

    Gather service info all at once, threaded

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,211 @@
 extern crate clap;
 extern crate serde_json;
 extern crate wdsapi;
-
+extern crate rayon;
 
 use clap::{App, AppSettings, Arg, SubCommand};
+use rayon::prelude::*;
 use serde_json::ser::to_string_pretty;
-use wdsapi::{Credentials, NewCollection, NewEnvironment, create_collection,
-             create_environment, credentials_from_file, get_collection_detail,
-             get_collections, get_configurations, get_environments};
+use std::thread::{JoinHandle, spawn};
+use wdsapi::{Collection, Configuration, Credentials, Environment,
+             NewCollection, NewEnvironment, Status, create_collection,
+             create_environment, credentials_from_file, delete_environment,
+             get_collection_detail, get_collections, get_configurations,
+             get_environments};
 
-fn get_and_print_env_children(creds: &Credentials, env_id: &str) {
-    match get_configurations(creds, env_id) {
-        Ok(configs) => {
-            let mut first = true;
-            for conf in configs.configurations {
-                if first {
-                    first = false;
-                    println!("   Configurations: {}", conf.name)
-                } else {
-                    println!("                   {}", conf.name)
+#[derive(Clone, Debug)]
+struct EnvironmentInfo {
+    environment: Environment,
+    configurations: Vec<Configuration>,
+    collections: Vec<Collection>,
+}
+
+#[derive(Clone, Debug)]
+struct DiscoveryServiceInfo {
+    creds: Credentials,
+    environments: Vec<EnvironmentInfo>,
+}
+
+fn get_configurations_thread(creds: &Credentials,
+                             env_id: &str)
+                             -> JoinHandle<Vec<Configuration>> {
+    let creds = creds.clone();
+    let env_id = env_id.to_string();
+    spawn(move || {
+        get_configurations(&creds, &env_id)
+            .expect("Failed to get configuration information, in thread")
+            .configurations
+    })
+}
+
+fn get_collections_thread(creds: &Credentials,
+                          env_id: &str)
+                          -> JoinHandle<Vec<Collection>> {
+    let creds = creds.clone();
+    let env_id = env_id.to_string();
+    spawn(move || {
+        get_collections(&creds, &env_id)
+            .expect("Failed to get collection information, in thread")
+            .collections
+            .par_iter()
+            .map({
+                |col| {
+                    get_collection_detail(&creds, &env_id, &col.collection_id)
+                        .expect("Failed to get collection detail, in thread")
                 }
-            }
-        }
-        Err(e) => println!("   Failed to get configurations {}", e),
+            })
+            .weight_max()
+            .collect()
+    })
+}
+
+fn environment_info(creds: &Credentials, env: &Environment) -> EnvironmentInfo {
+    let env_id = env.environment_id.clone();
+    // launch threads for these API calls
+    let conf_thread = get_configurations_thread(creds, &env_id);
+    let col_thread = get_collections_thread(creds, &env_id);
+    // Gather the results from the threads
+    let configurations = conf_thread.join()
+                                    .expect("Failed to get configuration \
+                                             information");
+    let collections = col_thread.join()
+                                .expect("Failed to get collection information");
+    EnvironmentInfo {
+        environment: env.clone(),
+        configurations: configurations,
+        collections: collections,
     }
+}
 
-    match get_collections(creds, env_id) {
-        Ok(cols) => {
-            let mut first = true;
-            for col in cols.collections {
-                match get_collection_detail(creds, env_id, &col.collection_id) {
-                    Ok(detail) => {
-                        let counts = detail.document_counts.unwrap();
-                        let formatted_counts = if counts.failed > 0 {
-                            format!("{} available, {} processing, {} failed",
-                                    counts.available,
-                                    counts.processing,
-                                    counts.failed)
-                        } else if counts.processing >
-                                                         0 {
-                            format!("{} available, {} processing",
-                                    counts.available,
-                                    counts.processing)
-                        } else {
-                            format!("{} available", counts.available)
-                        };
+fn discovery_service_info(matches: &clap::ArgMatches) -> DiscoveryServiceInfo {
+    let creds_file = matches.value_of("credentials")
+                            .expect("Internal error: Missing credentials?");
+    let creds = credentials_from_file(creds_file).expect("Invalid credentials");
+    let environments = get_environments(&creds)
+        .expect("Failed to get environment information")
+        .environments
+        .par_iter()
+        .map({
+            |env| environment_info(&creds, env)
+        })
+        .weight_max()
+        .collect();
+    DiscoveryServiceInfo {
+        creds: creds,
+        environments: environments,
+    }
+}
 
-                        if first {
-                            first = false;
-                            println!("   Collections: {}, {}",
-                                     detail.name,
-                                     formatted_counts)
-                        } else {
-                            println!("                {}, {}",
-                                     detail.name,
-                                     formatted_counts)
-                        }
-                    }
-                    Err(e) => {
-                        println!("   Failed to get collection detail {}", e)
-                    }
-                }
-            }
+fn writable_environment(info: &DiscoveryServiceInfo) -> EnvironmentInfo {
+    let writables: Vec<EnvironmentInfo> =
+        info.environments
+            .clone()
+            .into_iter()
+            .filter({
+                |env| !env.environment.read_only
+            })
+            .collect();
+
+    assert!(writables.len() <= 1,
+            format!("Multiple writable environments found {:?}", writables));
+
+    writables.first()
+             .expect("No writable environment found")
+             .clone()
+}
+
+fn newest_collection(env: &EnvironmentInfo) -> Collection {
+    // This is a lie, for now...
+    env.collections
+       .clone()
+       .into_iter()
+       .last()
+       .expect("No collection found")
+       .clone()
+}
+
+fn configuration(env: &EnvironmentInfo,
+                 configuration_id: &str)
+                 -> Configuration {
+    // This is a lie, for now...
+    env.configurations
+       .clone()
+       .into_iter()
+       .filter({
+           |c| c.configuration_id == configuration_id
+       })
+       .last()
+       .expect("No configuration found")
+       .clone()
+}
+
+fn print_env_children(env: &EnvironmentInfo) {
+    let mut first = true;
+    let configs: &Vec<Configuration> = &env.configurations;
+    let collections: &Vec<Collection> = &env.collections;
+    for conf in configs {
+        if first {
+            first = false;
+            println!("   Configurations: {}", conf.name)
+        } else {
+            println!("                   {}", conf.name)
         }
-        Err(e) => println!("   Failed to get collections {}", e),
+    }
+    first = true;
+    for col in collections {
+        let counts = col.document_counts.clone().unwrap();
+        let formatted_counts = if counts.failed > 0 {
+            format!("{} available, {} processing, {} failed",
+                    counts.available,
+                    counts.processing,
+                    counts.failed)
+        } else if counts.processing > 0 {
+            format!("{} available, {} processing",
+                    counts.available,
+                    counts.processing)
+        } else {
+            format!("{} available", counts.available)
+        };
+
+        if first {
+            first = false;
+            println!("   Collections: {}, {}", col.name, formatted_counts)
+        } else {
+            println!("                {}, {}", col.name, formatted_counts)
+        }
     }
 }
 
 fn show(matches: &clap::ArgMatches) {
-    let creds_file = matches.value_of("credentials").unwrap();
-    let creds = credentials_from_file(creds_file).unwrap(); // FIXME
-
-    match get_environments(&creds) {
-        Ok(envs) => {
-            for env in envs.environments {
-                match env.index_capacity {
-                    Some(index_capacity) => {
-                        println!("Environment: {}, {} disk, {} memory",
-                                 env.name,
-                                 index_capacity.disk_usage.total,
-                                 index_capacity.memory_usage.total)
-                    }
-                    None => {
-                        println!("Environment: {}", env.name);
-                    }
+    let info = discovery_service_info(matches);
+    for env in info.environments {
+        let status = match env.environment.status {
+            Status::Pending => " - pending",
+            _ => {
+                if env.environment.read_only {
+                    " - read only"
+                } else {
+                    ""
                 }
-                get_and_print_env_children(&creds, &env.environment_id)
+            }
+        };
+        let capacity = env.environment.index_capacity.as_ref();
+        match capacity {
+            Some(index_capacity) => {
+                println!("Environment: {}, {} disk, {} memory{}",
+                         env.environment.name,
+                         index_capacity.disk_usage.total,
+                         index_capacity.memory_usage.total,
+                         status)
+            }
+            None => {
+                println!("Environment: {}{}", env.environment.name, status);
             }
         }
-        Err(e) => println!("Failed to get environments {}", e),
+        print_env_children(&env)
     }
 }
+
 
 // I suppose there is a standard library way to do this...
 fn optional_string(s: &Option<&str>) -> Option<String> {
@@ -120,27 +235,33 @@ fn cenv(matches: &clap::ArgMatches) {
     }
 }
 
+fn denv(matches: &clap::ArgMatches) {
+    let info = discovery_service_info(matches);
+    let env_id = writable_environment(&info).environment.environment_id;
+
+    match delete_environment(&info.creds, &env_id) {
+        Ok(response) => {
+            println!("{}",
+                     to_string_pretty(&response)
+                         .expect("Internal error: failed to format \
+                                  delete_environment response"))
+        }
+        Err(e) => println!("Failed to delete environment {}", e),
+    }
+}
+
 fn ccol(matches: &clap::ArgMatches) {
-    let creds_file = matches.value_of("credentials").unwrap();
-    let creds = credentials_from_file(creds_file).unwrap(); // FIXME
+    let info = discovery_service_info(matches);
+    let env_id = writable_environment(&info).environment.environment_id;
+
     let col_options = NewCollection {
         name: matches.value_of("name").unwrap().to_string(),
         description: optional_string(&matches.value_of("description")),
         configuration_id:
             optional_string(&matches.value_of("configuration_id")),
     };
-    let envs = get_environments(&creds)
-        .expect("Failed to get environment information")
-        .environments;
-    // The following code should be refactored into helper functions
-    let env_id = envs.into_iter()
-                     .filter({
-                         |env| !env.read_only
-                     })
-                     .last()
-                     .expect("No writable environment found")
-                     .environment_id;
-    match create_collection(&creds, &env_id, &col_options) {
+
+    match create_collection(&info.creds, &env_id, &col_options) {
         Ok(response) => {
             println!("{}",
                      to_string_pretty(&response)
@@ -152,63 +273,50 @@ fn ccol(matches: &clap::ArgMatches) {
 }
 
 fn crawler(matches: &clap::ArgMatches) {
-    let creds_file = matches.value_of("credentials").unwrap();
-    let creds = credentials_from_file(creds_file).unwrap(); // FIXME
-    let envs = get_environments(&creds)
-        .expect("Failed to get environment information")
-        .environments;
-    // The following code should be refactored into helper functions
-    let environment = envs.into_iter()
-                          .filter({
-                              |env| !env.read_only
-                          })
-                          .last()
-                          .expect("No writable environment found");
-    let collections = get_collections(&creds, &environment.environment_id)
-        .expect("Failed to get collection information")
-        .collections;
-    let collection = collections.into_iter()
-                                .last()
-                                .expect("No collection found");
+    let info = discovery_service_info(matches);
+    // The following statement should be refactored into helper functions
+    let env = writable_environment(&info);
+
+    let collection = newest_collection(&env);
+    let config = configuration(&env, &collection.configuration_id);
+
     println!("# discovery_service.conf \
               generated by https://github.com/bruceadams/wdsapi
 {{
-    environment_id   = \
-              {:?} # {}
+    environment_id   = {:?} # {}
     collection_id    = {:?} # {}
-    \
-              configuration_id = {:?}
+    configuration_id = {:?} # {}
+
     base_url = {:?}
     credentials {{
-            \
-              username = {:?}
+            username = {:?}
             password = {:?}
     }}
 
-    \
-              api_version = \"2016-11-07\"
-    \
-              concurrent_upload_connection_limit = 2
+    api_version = \"2016-11-07\"
+    concurrent_upload_connection_limit = 2
     http_timeout = 125
-    \
-              send_stats {{
+    send_stats {{
             jvm = true
             os  = true
-    \
-              }}
+    }}
 }}
 ",
-             environment.environment_id,
-             environment.name,
+             env.environment.environment_id,
+             env.environment.name,
              collection.collection_id,
              collection.name,
              collection.configuration_id,
-             creds.url,
-             creds.username,
-             creds.password);
+             config.name,
+             info.creds.url,
+             info.creds.username,
+             info.creds.password);
 }
 
 fn main() {
+    rayon::initialize(rayon::Configuration::new().set_num_threads(64))
+        .expect("Failed to initialize thread pool");
+
     let matches = App::new("wdscli")
         .about("Basic administration for Watson Discovery Service.")
         .setting(AppSettings::SubcommandRequired)
@@ -238,7 +346,7 @@ fn main() {
                 .takes_value(true)
                 .help("Description text for the environment.")))
         .subcommand(SubCommand::with_name("delete-environment")
-            .visible_alias("ce")
+            .visible_alias("de")
             .about("Delete an environment")
             .arg(Arg::with_name("credentials")
                 .required(true)
@@ -298,11 +406,9 @@ fn main() {
     match matches.subcommand() {
         ("show", Some(m)) => show(m),
         ("create-environment", Some(m)) => cenv(m),
+        ("delete-environment", Some(m)) => denv(m),
         ("create-collection", Some(m)) => ccol(m),
         ("crawler-configuration", Some(m)) => crawler(m),
         _ => println!("Not implemented yet; sorry!"),
     }
-
-    // let serialized =
-    // serde_json::to_string_pretty(&deserialized).unwrap();
 }
