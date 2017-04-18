311138074b50cda8fbff2a2f2c3147367c4f617a
Bruce Adams <bruce.adams@acm.org>
2017-04-18 15:32:53 -0400

    More complete `query` command

--- src/query.rs
+++ src/query.rs
@@ -1,6 +1,6 @@
 use clap;
 use info::discovery_service_info;
-use select::{select_collection, writable_environment};
+use select::{read_only_environment, select_collection, writable_environment};
 use serde_json::ser::to_string_pretty;
 use wdsapi::common::{Credentials, QueryParams};
 use wdsapi::query;
@@ -9,6 +9,9 @@ fn query_params(matches: &clap::ArgMatches) -> QueryParams {
     QueryParams {
         filter: matches.value_of("filter").map(|s| s.to_string()),
         query: matches.value_of("query").map(|s| s.to_string()),
+        natural_language_query: matches.value_of("natural_language_query")
+                                       .map(|s| s.to_string()),
+        passages: Some(matches.is_present("oldest")),
         aggregation: matches.value_of("aggregation").map(|s| s.to_string()),
         count: matches.value_of("count")
                       .unwrap_or("1")
@@ -17,12 +20,17 @@ fn query_params(matches: &clap::ArgMatches) -> QueryParams {
         return_hierarchy: matches.value_of("return_hierarchy")
                                  .map(|s| s.to_string()),
         offset: matches.value_of("offset").map(|s| s.parse::<u64>().unwrap()),
+        sort: matches.value_of("sort").map(|s| s.to_string()),
     }
 }
 
 pub fn query(creds: Credentials, matches: &clap::ArgMatches) {
     let info = discovery_service_info(creds);
-    let env_info = writable_environment(&info);
+    let env_info = if matches.is_present("read-only") {
+        read_only_environment(&info)
+    } else {
+        writable_environment(&info)
+    };
     let collection = select_collection(&env_info, matches);
     let env_id = env_info.environment_id;
     let params = query_params(matches);
